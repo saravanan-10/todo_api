de900084320dbde2ffe6607b2661c506f179f7c5
Saravanan Selvam <saravanan.selvam@cloud5.com>
2024-04-10 17:14:40 +0530

    gitignore changes

--- config/DB.js
+++ config/DB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 const mongoDb = async()=>{
-    const connect = await mongoose.connect('mongodb_url');
+    const connect = await mongoose.connect('mongodb+srv://saravanakumar719:nodelearning@cluster0.v7zkocc.mongodb.net/todo');
     console.log('Mongoodb Connected');
 };
 
