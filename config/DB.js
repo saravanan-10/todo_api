f72cb900a6db68a36ee633440425d966057c48e1
Saravanan Selvam <saravanan.selvam@cloud5.com>
2024-04-26 15:54:32 +0530

    password encryption added

--- config/DB.js
+++ config/DB.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 
 const mongoDb = async()=>{
-    //const connect = await mongoose.connect('mongodb+srv://saravanakumar719:nodelearning@cluster0.v7zkocc.mongodb.net/todo'); 
     const connect = await mongoose.connect(process.env.MONGO_DB_URI); 
     console.log('Mongoodb Connected');
 };
